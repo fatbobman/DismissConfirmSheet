e67c41d7f1347f35908319e84b50a3c051234a1e
东坡肘子 <55673881+fatbobman@users.noreply.github.com>
2020-09-15 15:11:28 +0800

    Initial Commit

---
+++ DismissConfirmSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DismissConfirmSheet
+//
+//  Created by Yang Xu on 2020/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DismissConfirmSheet/DismissConfirmSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DismissConfirmSheetApp.swift
+//  DismissConfirmSheet
+//
+//  Created by Yang Xu on 2020/9/15.
+//
+
+import SwiftUI
+
+@main
+struct DismissConfirmSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
